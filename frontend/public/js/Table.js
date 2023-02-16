9ad54fee9a3d1db07e8be1ada3bc0bda46d9a678
ed's m1pro <ed_m1pro@edmbp.local>
2023-02-17 01:47:15 +0900

    2023-02-17 home

--- frontend/public/js/Table.js
+++ frontend/public/js/Table.js
@@ -3,27 +3,34 @@ import { Create, makeTable, tableBody } from "./Creators.js"
 const testBtnClick = (e) =>{
 	console.log(e.target.value)
 }
+const testCheckClick = (e) =>{
+	console.log(e.target.checked)
+}
 
 const testTable = [
 	{
 		id : 1,
 		name : "hosang",
 		age : 45,
+		complete : true,
 	},
 	{
 		id : 2,
 		name : "jihye",
 		age : 44,
+		complete : false,
 	},
 	{
 		id : 3,
 		name : "yaeeun",
 		age : 24,
+		complete : false,
 	},
 	{
 		id : 4,
 		name : "hyang",
 		age : 37,
+		complete : true,
 	},
 ]
 
@@ -38,10 +45,31 @@ const testBtn = {
 		click : testBtnClick,
 	}
 }
+const switchForm = {
+	element : "form",
+	classes : ["form-check", "form-switch"]
+}
+const testCheck = {
+	element : "input",
+	classes : ["form-check-input"],
+	attrs : {
+		value : null,
+		role : "switch",
+		type : "checkbox"
+	},
+	actions : {
+		click : testCheckClick,
+	}
+}
 
 for(let i = 0 ; i < testTable.length ; i++){
 	testBtn.attrs.value = testTable[i].id
 	testTable[i].edit = (new Create(testBtn)).element
+	const checkVal = testTable[i].complete
+	testTable[i].complete = (new Create(switchForm)).element
+	const checkEl = (new Create(testCheck)).element
+	checkVal ? checkEl.setAttribute("checked", true) : null
+	testTable[i].complete.appendChild(checkEl)
 }
 
 
